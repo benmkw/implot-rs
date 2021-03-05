17abc3c8e6e0918cb0c5068be197542bb2937165
Benedikt Mandelkow <benedikt.mandelkow@rwth-aachen.de>
2021-03-05 22:47:38 +0100

    minimize crash
    uncomment implot-examples/examples-shared/src/lib.rs:16 to see it crash
    
    maybe it happens around
    
                    // Set scissors on the renderpass.
                    let scissors = (
                        clip_rect[0].max(0.0).floor() as u32,                // left
                        clip_rect[1].max(0.0).floor() as u32,                // up
                        ((clip_rect[2] - clip_rect[0]).abs()).ceil() as u32, // right
                        ((clip_rect[3] - clip_rect[1]).abs()).ceil() as u32, // down
                    );
    in wgpu-imgui but I now think its more likely that the draw buffer might be bad?

--- implot-examples/examples-shared/src/lib.rs
+++ implot-examples/examples-shared/src/lib.rs
@@ -13,40 +13,7 @@ pub fn show_demos(ui: &Ui, plot_ui: &PlotUi) {
     Window::new(im_str!("implot-rs demo"))
         .size([430.0, 450.0], Condition::FirstUseEver)
         .build(ui, || {
-            ui.text(im_str!("Hello from implot-rs!"));
-            ui.text_wrapped(im_str!(
-                "The headers here demo the plotting features of the library.\
-                 Have a look at the example source code to see how they are implemented.\n\
-                 Check out the demo from ImPlot itself first for instructions on how to\
-                 interact with ImPlot plots."
-            ));
-
-            ui.separator();
-            ui.text(im_str!("Bar plots:"));
+            // ui.text(im_str!("Bar plots:")); // uncomment this to make it crash
             bar_plots::show_demo_headers(ui, plot_ui);
-
-            ui.separator();
-            ui.text(im_str!("Line plots:"));
-            line_plots::show_demo_headers(ui, plot_ui);
-
-            ui.separator();
-            ui.text(im_str!("Scatter plots:"));
-            scatter_plots::show_demo_headers(ui, plot_ui);
-
-            ui.separator();
-            ui.text(im_str!("Text plots:"));
-            text_plots::show_demo_headers(ui, plot_ui);
-
-            ui.separator();
-            ui.text(im_str!("Stairs plots:"));
-            stairs_plots::show_demo_headers(ui, plot_ui);
-
-            ui.separator();
-            ui.text(im_str!("Heatmaps:"));
-            heatmaps::show_demo_headers(ui, plot_ui);
-
-            ui.separator();
-            ui.text(im_str!("Stem plots:"));
-            stem_plots::show_demo_headers(ui, plot_ui);
         });
 }
--- implot-examples/implot-wgpu-demo/src/main.rs
+++ implot-examples/implot-wgpu-demo/src/main.rs
@@ -1,4 +1,3 @@
-
 use imgui::{im_str, Condition, Window};
 use implot::Context;
 
@@ -21,24 +20,5 @@ fn main() {
         if showing_rust_demo {
             examples_shared::show_demos(ui, &plot_ui);
         }
-
-        Window::new(im_str!("Welcome to the ImPlot-rs demo!"))
-            .size([430.0, 450.0], Condition::FirstUseEver)
-            .build(ui, || {
-                ui.checkbox(im_str!("Show C++ ImPlot demo window"), &mut showing_demo);
-                ui.checkbox(
-                    im_str!("Show Rust ImPlot demo windows"),
-                    &mut showing_rust_demo,
-                );
-                // TODO(4bb4) ... move windows by default so this is less confusing
-                ui.text_wrapped(im_str!(
-                    "Note that the windows are stacked, so move this one out of the way to see\
-                     the ones beneath it. If you see something in the C++ demo window, but not\
-                     in the Rust ImPlot demo window, that means the bindings are likely not   \
-                     implemented yet. Feel free to open an issue if you are missing something \
-                     in particular.
-                    "
-                ));
-            });
     });
 }
